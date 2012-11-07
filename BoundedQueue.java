ed07072d98ced02c426734e81a968e536e540ac3
Boyang Zhang <boyang.d.zhang@gmail.com>
2012-11-07 12:25:20 -0500

    readme and stuff

--- BoundedQueue.java
+++ BoundedQueue.java
@@ -5,6 +5,12 @@ public class BoundedQueue {
     int tail;
     int size;
 
+    /*
+     * BoundedQueue - constructor for BoundedQueue class
+     *
+     * @param int length - length of queue
+     */
+
     public BoundedQueue(int length ) {
         queue = new int[length];
         head = 0;
@@ -12,13 +18,33 @@ public class BoundedQueue {
         size = 0;
     }
 
+    /*
+     * isEmpty - checks to see if Queue is empty
+     *
+     * @return true if empty, false if not
+     */
+
     public boolean isEmpty() {
         return size == 0;
     }
 
+    /*
+     * isFull - checks to see if Queue is full
+     *
+     * @return true if full, false if not
+     */
+
     public boolean isFull() {
         return size == queue.length;
     }
+
+    /*
+     * enqueue - Enqueue a value into the BoundedQueue.  
+     * Prints "Queue is full" and returns if attempting to 
+     * enqueue into a full queue.
+     *
+     * @param int n - value to enqueue
+     */
 
     public void enqueue(int n) {
         if(isFull()) {
@@ -33,11 +59,18 @@ public class BoundedQueue {
         }
     }
 
-    public int dequeue() {
+    /*
+     * dequeue - Dequeues the current head value from BoundedQueue.  
+     * Prints "Queue is empty" and throws an Exception
+     * if attempting to dequeue from an empty queue.
+     *
+     * @return enqueued int value in FIFO order
+     */
+
+    public int dequeue() throws Exception{
         int retVal = 0;
         if(isEmpty()) {
-            System.out.println("Queue is empty");
-            return 0;
+            throw new Exception("Queue is empty");
         }
         else {
             if(head == queue.length) head = 0;
@@ -49,13 +82,20 @@ public class BoundedQueue {
        return retVal;   
     }
 
+    /*
+     * main - basic tests
+     */
     public static void main(String args[]) {
-        System.out.println("Intiialize zero length queue, enqueue and dequeue");
+        System.out.println("Try zero length");
         BoundedQueue bq = new BoundedQueue(0);
         bq.enqueue(5);
-        bq.dequeue();
-
-        System.out.println("Initialize a 5 length queue,  Enqueue 6 times and dequeue 6 times");
+        try{
+            bq.dequeue();
+        }
+        catch(Exception e) {
+            System.out.println("Dequeue Exception thrown on empty queue!");
+        }
+        System.out.println("Try 5 length");
         BoundedQueue bq1 = new BoundedQueue(5);
         bq1.enqueue(1);
         bq1.enqueue(2);
@@ -64,36 +104,62 @@ public class BoundedQueue {
         bq1.enqueue(5);
         bq1.enqueue(6);
         for(int i = 0; i < 6; i++) {
-            System.out.println(bq1.dequeue());
+            try {
+                System.out.println(bq1.dequeue());
+            }
+            catch(Exception e) {
+                System.out.println("Dequeue Exception thrown on empty queue!");
+            }
+
         }
 
         BoundedQueue bq2 = new BoundedQueue(4);
-        System.out.println("Enqueuing");
         bq2.enqueue(4);
         bq2.enqueue(5);
-        System.out.println(bq2.dequeue());
-        System.out.println("Enqueuing");
+        try {
+            System.out.println(bq2.dequeue());
+        }
+        catch(Exception e) {
+            System.out.println("Dequeue Exception thrown on empty queue!");
+        }
         bq2.enqueue(7);
         bq2.enqueue(6);
         bq2.enqueue(8);
         bq2.enqueue(9);
-        System.out.println(bq2.dequeue());
-        System.out.println(bq2.dequeue()); 
-        System.out.println(bq2.dequeue());
-        System.out.println(bq2.dequeue());
-        System.out.println(bq2.dequeue());
-        System.out.println("Enqueuing");
+        try {
+            System.out.println(bq2.dequeue());
+            System.out.println(bq2.dequeue()); 
+            System.out.println(bq2.dequeue());
+            System.out.println(bq2.dequeue());
+            System.out.println(bq2.dequeue());
+        }
+        catch(Exception e) {
+            System.out.println("Dequeue Exception thrown on empty queue!");
+        }
         bq2.enqueue(1);
-        System.out.println(bq2.dequeue());
-        System.out.println(bq2.dequeue());
-        System.out.println("Enqueuing");
+        try {
+            System.out.println(bq2.dequeue());
+            System.out.println(bq2.dequeue());
+        }
+        catch(Exception e) {
+            System.out.println("Dequeue Exception thrown on empty queue!");
+        }        
         bq2.enqueue(4);
         bq2.enqueue(5);
         bq2.enqueue(6);
-        System.out.println(bq2.dequeue());
-        System.out.println(bq2.dequeue());
-        System.out.println("Enqueuing");
+        try {
+            System.out.println(bq2.dequeue());
+            System.out.println(bq2.dequeue());
+        }
+        catch(Exception e) {
+            System.out.println("Dequeue Exception thrown on empty queue!");
+        }        
         bq2.enqueue(8);
-        System.out.println(bq2.dequeue());
+        try {
+            System.out.println(bq2.dequeue());
+        }
+        catch(Exception e) {
+            System.out.println("Dequeue Exception thrown on empty queue!");
+        }        
     }
 }
---
+++ README.md
@@ -0,0 +1,16 @@
+BoundedQueue.java
+-------------
+
+Methods
+--------
+BoundedQueue(int length) - constructor
+isEmpty() - test if queue is empty
+isFull() - test if queue is full
+enqueue(int n) - enqueues a value
+dequeue() - dequeues a value
+
+How to run
+-----------
+./runQueue.sh
+
+Compiles and runs the basic test main
